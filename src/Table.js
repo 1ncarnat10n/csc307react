4102d841431cb0d9e9916524c7b70b653e9e1202
1ncarnat10n <tsaiwilliam888@gmail.com>
2022-01-07 13:41:03 -0800

    Updated

--- src/Table.js
+++ src/Table.js
@@ -14,10 +14,13 @@ function TableHeader()  {
   function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
-        <tr key={index}>
-          <td>{row.name}</td>         
-          <td>{row.job}</td>
-        </tr>
+<tr key={index}>
+  <td>{row.name}</td>
+  <td>{row.job}</td>
+  <td>
+    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+  </td>
+</tr>
       );
      }
     );
@@ -28,13 +31,13 @@ function TableHeader()  {
      );
   }
 
-  function Table(props) {
+  function Table (props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
       </table>
     );
-} 
+  }
 
 export default Table;
